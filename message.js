ab592311b8d793f95cc8eb9e9639cbf23f9a3509
Bishwarup Dutta <bishwarupdutta.bd@gmail.com>
2020-05-10 18:14:21 +0530

    practise day

--- message.js
+++ message.js
@@ -7,7 +7,7 @@ console.log("Welcome to Javascript Practise session");
 /*var num = 100;
 console.log(num);*/
 
-let myNumber = 36;
+/*let myNumber = 36;
 let myName = 'Bishwarup';
 let status = true;
 let studentDetails = null;
@@ -16,4 +16,109 @@ console.log('myNumber', myNumber, typeof myNumber);
 console.log('myName', myName, typeof myName);
 console.log('status', status, typeof status);
 console.log('studentDetails', studentDetails, typeof studentDetails);
-console.log('myErr', myErr, typeof myErr);
+console.log('myErr', myErr, typeof myErr);*/
+
+
+/*var x = 20;
+var y = 10;
+var z = x + y;
+//document.write(z); // Use of document.write is to write the HTML expressions or Javascript code to a document.
+console.log(z);
+*/
+/*var data = 200;//gloabal variable
+function a() {
+    document.writeln(data);
+}
+function b() {
+    document.writeln(data);
+}
+a();//calling JavaScript function
+b();
+*/
+
+//alert 
+
+/*function logout() {
+    console.log("You are log out");
+}
+//alert("you are logout");
+logout();
+*/
+
+/*let value = 50;
+function a() {
+    alert(value);
+}
+a();
+console.log(value);*/
+
+/*window.value = 90;
+function m() {
+    window.value = 100;
+
+}
+function n() {
+    alert(window.value);
+}
+m();
+n();
+*/
+/*
+var a = 30;
+var b = 'bishwarup';
+var c = false;
+var d = null;
+var e = undefined;
+console.log(a, typeof (a));
+console.log(typeof (b));
+console.log(typeof (e));*/
+
+// operators
+
+/*let num = 12;
+num++;
+console.log(num);
+*/
+
+/*let num = 20;
+if (num > 10) {
+    console.log("Value num is greater than 10 ");
+}
+*/
+
+/*var a = 20;
+if (a % 2 == 0) {
+    console.log("a is even number");
+}
+else {
+    console.log("a is odd number");
+}
+
+
+var a = 34;
+if (a == 10) {
+    console.log("a is equal to 10");
+}
+else if (a == 15) {
+    console.log("a is equal to 15");
+}
+else if (a == 20) {
+    console.log("a is equal to 20");
+}
+else {
+    console.log("a is not equal to 10, 15 or 20");
+}*/
+
+var numberObj = new Number(50);
+console.log(numberObj.valueOf());
+
+
+var count = 45;
+console.log(count.toString());
+console.log(count.toString(2));
+
+console.log((255).toString(16));
+
+
+
+
