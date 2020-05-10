b4730716bf92d50cf260126532affdabfde5dd8e
Bishwarup Dutta <bishwarupdutta.bd@gmail.com>
2020-05-10 14:28:46 +0530

    practise day

---
+++ message.js
@@ -0,0 +1,19 @@
+function msg() {
+    alert("Hello JavaTpoint");
+}
+console.log("Welcome to Javascript Practise session");
+
+
+/*var num = 100;
+console.log(num);*/
+
+let myNumber = 36;
+let myName = 'Bishwarup';
+let status = true;
+let studentDetails = null;
+let myErr = undefined;
+console.log('myNumber', myNumber, typeof myNumber);
+console.log('myName', myName, typeof myName);
+console.log('status', status, typeof status);
+console.log('studentDetails', studentDetails, typeof studentDetails);
+console.log('myErr', myErr, typeof myErr);
