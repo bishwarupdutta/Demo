128adf6cb04380a971389d1a2bce8af0f0410195
Bishwarup Dutta <bishwarupdutta.bd@gmail.com>
2020-05-11 13:16:52 +0530

    practise day

--- javascript.js
+++ javascript.js
@@ -53,8 +53,73 @@ accessForDL = (age >= 18) ? 'Issue Driving Licence ' : ' Don\'t Issue ';
 
 console.log(accessForDL);*/
 
-var grade;
-var marks = prompt("Enter your marks", "");
+//var grade;
+//var marks = prompt("Enter your marks", "");
 
-grade = (marks > 90) ? 'Grade E' : (marks > 80) ? 'Grade A' : (marks > 70) ? 'Grade B' : (marks > 50) ? 'Grade C' : (marks > 40) ? 'Grade D' : 'Fail';
-console.log(grade);
+//grade = (marks > 90) ? 'Grade E' : (marks > 80) ? 'Grade A' : (marks > 70) ? 'Grade B' : (marks > 50) ? 'Grade C' : (marks > 40) ? 'Grade D' : 'Fail';
+//console.log(grade);
+
+
+//switch statement
+
+var grade = 'B';
+switch (grade) {
+    case 'A':
+        console.log('Excellent');
+        break;
+    case 'B':
+        console.log('Well done');
+        break;
+    case 'C':
+        console.log('Just pass');
+        break;
+    default:
+        console.log('Better try next time');
+        break;
+
+}
+
+//while and do-while statement
+
+var count = 0;
+var total = 0;
+
+console.log('Count:  Total:');
+while (count < 3) {
+    count++;
+    total += count;      // total = total +count
+    console.log(count + '\t\t' + total);
+}
+
+var count = 0;
+
+do {
+    count += 1;   // count =count +1
+    console.log(count);
+} while (count > 5);
+
+//for statement
+
+for (var count = 0; count < 3; count++) {
+    console.log('Value: ' + count);
+}
+
+//break statement
+for (var count = 1; count < 10; count++) {
+    if (count % 2 == 0) {
+        break;
+    }
+    console.log('Value: ' + count);
+
+}
+
+//continue statement
+
+for (var count = 1; count < 10; count++) {
+    if (count % 2 == 0) {
+        continue;
+    }
+    console.log('Value: ' + count);
+
+}
+
