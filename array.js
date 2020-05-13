416b81e85740f9a376a986123d5b7229018e2993
Bishwarup Dutta <bishwarupdutta.bd@gmail.com>
2020-05-13 21:08:11 +0530

    practise day

--- array.js
+++ array.js
@@ -1,7 +1,33 @@
 'use strict';
 
+
+var ename = 'Welcome to Javascript Session';
+console.log('Session -> ', ename);
+
 // Shallow Copy and Deep copy
 
+var teamA = [1, 2, 3, 4];
+var teamB = [];
 
+teamB = teamA;
 
+teamA[0] = 10;
+
+console.log('teamA ->' + teamA);
+console.log('teamB ->' + teamB);
+
+//Deep COpy
+
+var teamA = [5, 6, 7, 8];
+var teamB = new Array(teamA.length);
+
+for (var i = 0; i < teamB.length; i++) {
+    teamB[i] = teamA[i];
+}
+
+teamA[0] = 10;
+teamA[1] = 20;
+
+console.log('TeamA ->' + teamA); // [10,20,7,8]
+console.log('TeamB ->' + teamB); // [5,6,7,8]
 
